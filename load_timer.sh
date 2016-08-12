66dc759ec732018b700e9c7943444682d08c4828
Geert Folkertsma <geert@geertfolkertsma.nl>
2016-08-12 13:14:15 +0200

    changes epiphany to firefox for svg support; adds vnc install instruction

--- load_timer.sh
+++ load_timer.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 #/home/pi/load_timer.sh
 
-epiphany -a --profile /home/pi/.config http://ram-sc.roaming.utwente.nl:3003 >/dev/null 2>&1 &
+firefox http://ram-sc.roaming.utwente.nl:3003 >/dev/null 2>&1 &
 unclutter -display :0 -noevents -grab >/dev/null 2>&1 &
 sleep 15 && xte "key F11" -x:0
